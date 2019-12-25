cc7cf931b102c4e31e1af64da94a3c5dd8d1e6b9
Emrah Budur <emrah.budur@yahoo.com>
2019-12-26 00:40:05 +0300

    fixed token_type_ids

--- bertviz/attention.py
+++ bertviz/attention.py
@@ -65,7 +65,7 @@ def get_attention(model, model_type, tokenizer, sentence_a, sentence_b=None, inc
 			tokens_a = [tokenizer.cls_token] + tokenizer.tokenize(sentence_a) + [
 				tokenizer.sep_token]
 			tokens_b = tokenizer.tokenize(sentence_b) + [tokenizer.sep_token] + [tokenizer.mlb_token]
-			token_type_ids = torch.LongTensor([[0] * len(tokens_a) + [1] * len(tokens_b)])
+			token_type_ids =  len(1 + tokens_a + 1) * [0] + len(tokens_b + 1 + 1 + 1) * [1]
 		elif model_type == 'roberta':
 			tokens_a = [tokenizer.cls_token] + tokenizer.tokenize(sentence_a) + [tokenizer.sep_token]
 			tokens_b = [tokenizer.sep_token] + tokenizer.tokenize(sentence_b) + [tokenizer.sep_token]
