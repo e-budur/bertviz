86fd64dca01979adb12e6b2f4174baf4ea047a3c
Emrah Budur <emrah.budur@yahoo.com>
2019-12-29 15:28:04 +0300

    debugging bert-nlu visualizer

---
+++ unit_test/TestingBertNLUVisualizer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+# import modules used here -- sys is a very standard one
+import sys
+from bertviz.pytorch_transformers_attn import BertModel, BertTokenizer
+from bertviz.head_view import show
+from transformers.tokenization_bert_nlu import BertNLUTokenizer
+from transformers.tokenization_bert import BertTokenizer
+from transformers.modeling_bert import BertForPreTraining as BertModel
+from transformers.modeling_bert_nlu import BertNLUForPreTraining as BertNLUModel
+from bertviz.head_view import show
+
+def test_bert():
+	model_type = 'bert'
+	model_version = 'bert-base-cased'
+	do_lower_case = False
+	model = BertModel.from_pretrained(model_version, output_attentions=True)
+	tokenizer = BertTokenizer.from_pretrained(model_version, do_lower_case=do_lower_case)
+	sentence_a = "The cat sat on the mat"
+	sentence_b = "The cat lay on the rug"
+	show(model, model_type, tokenizer, sentence_a, sentence_b)
+
+def test_bert_nlu():
+	model_type = 'bert-nlu'
+
+	model_path = '../transformers/output/pretrain/bert-nlu-gpu/checkpoint-50000'
+	model_version = 'bert-base-cased'
+
+	do_lower_case = False
+	model = BertNLUModel.from_pretrained(model_path, output_attentions=True)
+	tokenizer = BertNLUTokenizer.from_pretrained(model_version, do_lower_case=do_lower_case)
+	sentence_a = "The cat sat on the mat"
+	sentence_b = "The cat lay on the rug"
+	show(model, model_type, tokenizer, sentence_a, sentence_b=sentence_b)
+# Gather our code in a main() function
+def main():
+	test_bert()
+
+
+  # Command line args are in sys.argv[1], sys.argv[2] ..
+  # sys.argv[0] is the script name itself and can be ignored
+
+# Standard boilerplate to call the main() function to begin
+# the program.
+if __name__ == '__main__':
+  main()
