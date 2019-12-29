ab47416b4f8697297af117b26ae321c8e38d2cde
Emrah Budur <emrah.budur@yahoo.com>
2019-12-29 14:46:19 +0300

    fixed the sentence featurizer step in the bert-nlu visualizer

--- bertviz/attention.py
+++ bertviz/attention.py
@@ -51,7 +51,7 @@ def get_attention(model, model_type, tokenizer, sentence_a, sentence_b=None, inc
 			tokens_a = [tokenizer.cls_token] + tokenizer.tokenize(sentence_a) + [tokenizer.sep_token]
 		elif model_type == 'bert-nlu':
 			tokens_a = [tokenizer.cls_token] + [tokenizer.mlb_token] + tokenizer.tokenize(sentence_a) + [
-				tokenizer.sep_token]  + [tokenizer.sep_token]
+				tokenizer.sep_token]
 		elif model_type == 'xlnet':
 			tokens_a = tokenizer.tokenize(sentence_a) + [tokenizer.sep_token] + [tokenizer.cls_token]
 		else:
@@ -64,7 +64,7 @@ def get_attention(model, model_type, tokenizer, sentence_a, sentence_b=None, inc
 		elif model_type == 'bert-nlu':
 			tokens_a = [tokenizer.cls_token] + [tokenizer.mlb_token] + tokenizer.tokenize(sentence_a) + [
 				tokenizer.sep_token]
-			tokens_b = tokenizer.tokenize(sentence_b) + [tokenizer.sep_token] + [tokenizer.sep_token]
+			tokens_b = tokenizer.tokenize(sentence_b) + [tokenizer.sep_token]
 			token_type_ids =  torch.LongTensor([[0] * len(tokens_a) + [1] * len(tokens_b)])
 		elif model_type == 'roberta':
 			tokens_a = [tokenizer.cls_token] + tokenizer.tokenize(sentence_a) + [tokenizer.sep_token]
